4f7fd6ba579d51ae4d6ba96d25815dae6c403d67
sjemmeh <jaimie@live.nl>
2022-02-17 06:28:54 +0100

    sql cleanup

--- structure.sql
+++ structure.sql
@@ -1,31 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: db
--- Gegenereerd op: 17 feb 2022 om 05:23
--- Serverversie: 10.6.5-MariaDB-1:10.6.5+maria~focal
--- PHP-versie: 8.0.15
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `data`
---
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `authorized_users`
---
 
 CREATE TABLE `authorized_users` (
   `id` int(11) NOT NULL,
@@ -34,12 +6,6 @@ CREATE TABLE `authorized_users` (
   `authorized` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `messages`
---
-
 CREATE TABLE `messages` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
@@ -47,12 +13,6 @@ CREATE TABLE `messages` (
   `archived` int(11) NOT NULL DEFAULT 0,
   `date` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `orders`
---
 
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
@@ -65,75 +25,33 @@ CREATE TABLE `orders` (
   `date` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `products`
---
-
 CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `product` varchar(255) NOT NULL,
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexen voor geëxporteerde tabellen
---
-
---
--- Indexen voor tabel `authorized_users`
---
 ALTER TABLE `authorized_users`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexen voor tabel `messages`
---
 ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexen voor tabel `orders`
---
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexen voor tabel `products`
---
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT voor geëxporteerde tabellen
---
-
---
--- AUTO_INCREMENT voor een tabel `authorized_users`
---
 ALTER TABLE `authorized_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT voor een tabel `messages`
---
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT voor een tabel `orders`
---
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT voor een tabel `products`
---
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
